31db5ce160581cffa450e1a2b80e1287f55fb15c
TienLe26dev <letien26102003@gmail.com>
2025-05-14 11:05:34 +0700

    TienLe

--- src/main/java/com/husc/lms/controller/DocumentController.java
+++ src/main/java/com/husc/lms/controller/DocumentController.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,7 +39,7 @@ public class DocumentController {
 	private DocumentService documentService;
 	
 	@PostMapping("/create")
-	public APIResponse<DocumentResponse> createDocument(@RequestBody @Valid DocumentRequest request){
+	public APIResponse<DocumentResponse> createDocument(@ModelAttribute @Valid DocumentRequest request){
 		return APIResponse.<DocumentResponse>builder()
 				.result(documentService.createDocument(request))
 				.build();
